c40d96c23693f682c070375e8091d5448eb52c07
devvMuhammad <muhammadaljoufi@gmail.com>
2024-03-31 02:54:45 +0500

    feat: shadcn-ui setup

---
+++ src/app/colors.tsx
@@ -0,0 +1,24 @@
+export default function page() {
+  return (
+    <div>
+      <h1 className="p-2 h-screen bg-[#001021] text-white">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        fugit molestiae amet, atque quisquam explicabo aliquam possimus
+        reprehenderit quos eaque iste repudiandae, omnis rem consectetur totam
+        sit optio neque praesentium debitis laudantium iusto enim non.
+        Architecto repellat, accusantium ab doloribus consequatur maxime ipsum,
+        omnis unde eaque minima facilis temporibus numquam?
+        <button className="block hover:bg-opacity-90 h-8 w-20 bg-[#00264f] rounded-md text-white">
+          Click
+        </button>
+        <button className="block hover:bg-opacity-90 h-8 w-20 bg-[#023e8a] rounded-md text-white">
+          Click
+        </button>
+        <button className="block hover:bg-opacity-90 h-8 w-20 bg-[#ff8600] rounded-md text-white">
+          Click
+        </button>
+      </h1>
+    </div>
+  );
+}
