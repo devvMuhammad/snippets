b0abde3a881f33edcbaab03f3067fcc55d05cc1c
muhammad <muhammadaljoufi@gmail.com>
2024-04-02 01:46:12 +0500

    feat: small ui fixes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
           New
         </Button>
       </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
         <SnippetOverview />
         <SnippetOverview />
         <SnippetOverview />
