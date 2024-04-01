00a862468f900668d3311b0a032a39c89301d8eb
muhammad <muhammadaljoufi@gmail.com>
2024-04-02 01:46:01 +0500

    feat: added more icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,7 +1,25 @@
-import { Code, Plus, Rocket, Settings } from "lucide-react";
+import {
+  Calendar,
+  Code,
+  EllipsisVertical,
+  EyeIcon,
+  HeartIcon,
+  Loader2,
+  Plus,
+  Rocket,
+  Settings,
+  Trash,
+} from "lucide-react";
+
 export const Icons = {
   code: Code,
   rocket: Rocket,
   settings: Settings,
   add: Plus,
+  ellipsis: EllipsisVertical,
+  spinner: Loader2,
+  trash: Trash,
+  calender: Calendar,
+  eye: EyeIcon,
+  likes: HeartIcon,
 };
