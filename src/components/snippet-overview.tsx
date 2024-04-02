7453fa6e5d1342da24b757bace3d1513ad5dbbdd
devvMuhammad <muhammadaljoufi@gmail.com>
2024-04-02 19:38:34 +0500

    feat: added tooltips to show analytic type

--- src/components/snippet-overview.tsx
+++ src/components/snippet-overview.tsx
@@ -1,25 +1,18 @@
 import {
-  BellIcon,
-  CalendarIcon,
-  CopyIcon,
-  EyeIcon,
-  FileEditIcon,
-  Link,
-  MoreHorizontalIcon,
-  Package,
-  TrashIcon,
-} from "lucide-react";
-import { Button } from "./ui/button";
-import {
   Card,
   CardContent,
   CardDescription,
   CardFooter,
-  CardHeader,
   CardTitle,
 } from "./ui/card";
 import { SnippetOperations } from "./snippet-operations";
 import { Icons } from "./icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 type SnippetOverviewProps = {
   title: string;
@@ -45,15 +38,9 @@ export default function SnippetOverview({
       {/* CARD CONTENT */}
       <CardContent className="w-full grid grid-cols-[1fr_auto] gap-6 justify-between items-center p-4 md:p-6">
         <div className="grid gap-1">
-          <CardTitle className="text-lg">
-            Integrating with Shadcn Lorem ipsum
-          </CardTitle>
+          <CardTitle className="text-lg">{title}</CardTitle>
           <CardDescription className="overflow-hidden text-nowrap overflow-ellipsis">
-            Lorem ipsum dol Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Animi sit, aliquid repellat voluptatum quae repudiandae illum
-            beatae quis expedita facere voluptatem minima officiis cum et magni
-            excepturi at quas tempore dolore similique dolorem. Hic qui dolorem
-            deleniti reprehenderit perspiciatis quam.
+            {description}
           </CardDescription>
         </div>
         {/* <Button size="sm">View</Button> */}
@@ -61,26 +48,50 @@ export default function SnippetOverview({
       </CardContent>
       {/* CARD FOOTER */}
       <CardFooter className="border-t p-4">
-        <div className="w-full flex justify-evenly items-center gap-4">
-          <div className="flex items-center gap-2">
-            <Icons.calender className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-blue-500 cursor-pointer" />
-            <span className="text-xs text-gray-500 dark:text-gray-400">
-              2 days
-            </span>
+        <TooltipProvider delayDuration={0}>
+          <div className="w-full flex justify-evenly items-center gap-4">
+            <Tooltip>
+              <div className="flex items-center gap-2">
+                <TooltipTrigger>
+                  <Icons.calender className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-blue-500 cursor-pointer" />
+                </TooltipTrigger>
+                <TooltipContent className="text-blue-500 border-none text-xs px-2 py-1">
+                  Date
+                </TooltipContent>
+                <span className="text-xs text-gray-500 dark:text-gray-400">
+                  {/* 2 days ago */}
+                  {new Date().toLocaleDateString()}
+                </span>
+              </div>
+            </Tooltip>
+            <Tooltip>
+              <div className="flex items-center gap-2">
+                <TooltipTrigger>
+                  <Icons.eye className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-green-500 cursor-pointer" />
+                </TooltipTrigger>
+                <TooltipContent className="text-green-600 border-none text-xs px-2 py-1">
+                  Likes
+                </TooltipContent>
+                <span className="text-xs text-gray-500 dark:text-gray-400">
+                  100
+                </span>
+              </div>
+            </Tooltip>
+            <Tooltip>
+              <div className="flex items-center gap-2">
+                <TooltipTrigger>
+                  <Icons.likes className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-red-600 cursor-pointer" />
+                </TooltipTrigger>
+                <TooltipContent className="text-red-600 border-none text-xs px-2 py-1">
+                  Date
+                </TooltipContent>
+                <span className="text-xs text-gray-500 dark:text-gray-400">
+                  48
+                </span>
+              </div>
+            </Tooltip>
           </div>
-          <div className="flex items-center gap-2">
-            <Icons.eye className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-blue-500 cursor-pointer" />
-            <span className="text-xs text-gray-500 dark:text-gray-400">
-              100 <span className="hidden md:inline"> Views</span>
-            </span>
-          </div>
-          <div className="flex items-center gap-2">
-            <Icons.likes className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-red-600 cursor-pointer" />
-            <span className="text-xs text-gray-500 dark:text-gray-400">
-              48 <span className="hidden md:inline">Likes</span>
-            </span>
-          </div>
-        </div>
+        </TooltipProvider>
       </CardFooter>
     </Card>
   );
